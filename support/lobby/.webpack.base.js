f82cb5d7cf81145387548c938a3da5efe1365557
David Easter <this@davideaster.com>
2017-11-18 18:12:49 -0500

    Babel

--- support/lobby/.webpack.base.js
+++ support/lobby/.webpack.base.js
@@ -17,6 +17,16 @@ module.exports = {
   module: {
     rules: [ {
       test:
+        /\.js$/,
+      exclude:
+        /node_modules/,
+      loader:
+        "babel-loader",
+      options: {
+        presets: [ "env" ],
+      },
+    }, {
+      test:
         /\.css$/,
       use:
         ExtractText.extract( { fallback: "style-loader", use: "css-loader" } ),
