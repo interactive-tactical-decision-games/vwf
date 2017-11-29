41c81e465159afa5f07c3aa768a50a2716bee284
David Easter <this@davideaster.com>
2017-11-28 23:42:04 -0500

    layout

---
+++ support/lobby/lib/Layout.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { TabContainer, Navbar, Image, Nav, NavItem, TabContent, TabPane } from "react-bootstrap";
+
+export default function Layout( props ) {
+  return <TabContainer generateChildId={ ( key, type ) => type + "-" + key }>
+    <div>
+      <Navbar fluid collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Toggle
+            data-toggle="collapse" data-target="#navbar"/>
+          <Navbar.Brand>
+            <a href="#">
+              <span><Image className="logo_img_small" src="/ONR_Logo.jpg"/></span>
+              <span>&ensp;</span>
+              <span>Title</span>
+            </a>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Navbar.Collapse id="navbar">
+          <Nav>
+            <NavItem eventKey="scenarios">Scenarios</NavItem>
+            <NavItem eventKey="sessions">Sessions</NavItem>
+            <NavItem eventKey="review">Review</NavItem>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <TabContent animation={ false }>
+        <TabPane eventKey="scenarios">Scenarios</TabPane>
+        <TabPane eventKey="sessions">Sessions</TabPane>
+        <TabPane eventKey="review">Review</TabPane>
+      </TabContent>
+    </div>
+  </TabContainer>;
+}
