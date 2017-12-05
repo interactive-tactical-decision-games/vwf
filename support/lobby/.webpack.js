037a226d06a6c7261acb96bb0d7c566e72299ea6
David Easter <this@davideaster.com>
2017-12-05 09:24:10 -0500

    webpack bundle css

--- support/lobby/.webpack.js
+++ support/lobby/.webpack.js
@@ -1,16 +1,28 @@
 "use strict";
 
-const path = require( "path" );
+const path = require( "path" ),
+  ExtractText = require( "extract-text-webpack-plugin" );
 
 module.exports = {
   context:
     path.resolve( __dirname ),
   entry:
-    "./index.js",
+    [ "./index.js", "./index.css" ],
   output: {
     path: path.resolve( __dirname ),
     filename: "bundle.js",
   },
   devtool:
     "source-map",
+  module: {
+    rules: [ {
+      test:
+        /\.css$/,
+      use:
+        ExtractText.extract( { fallback: "style-loader", use: "css-loader" } ),
+    } ]
+  },
+  plugins: [
+    new ExtractText( { filename: "bundle.css" } ),
+  ]
 };
