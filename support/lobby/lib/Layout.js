506ea9daa27366a80a845cee1e58d246062a3d10
David Easter <david.easter@lmco.com>
2017-12-20 15:55:04 -0500

    layout as class

--- support/lobby/lib/Layout.js
+++ support/lobby/lib/Layout.js
@@ -5,34 +5,38 @@ import Scenarios from "./scenarios";
 import Sessions from "./sessions";
 import Review from "./review";
 
-export default function Layout( props ) {
-  return <TabContainer generateChildId={ ( key, type ) => type + "-" + key }>
-    <div>
-      <Navbar fluid collapseOnSelect>
-        <Navbar.Header>
-          <Navbar.Toggle
-            data-toggle="collapse" data-target="#navbar"/>
-          <Navbar.Brand>
-            <a href="#">
-              <span><Image className="logo_img_small" src="/ONR_Logo.jpg"/></span>
-              <span>&ensp;</span>
-              <span>Title</span>
-            </a>
-          </Navbar.Brand>
-        </Navbar.Header>
-        <Navbar.Collapse id="navbar">
-          <Nav>
-            <NavItem eventKey="scenarios">Scenarios</NavItem>
-            <NavItem eventKey="sessions">Sessions</NavItem>
-            <NavItem eventKey="review">Review</NavItem>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      <TabContent animation={ false }>
-        <TabPane eventKey="scenarios"><Scenarios/></TabPane>
-        <TabPane eventKey="sessions"><Sessions/></TabPane>
-        <TabPane eventKey="review"><Review/></TabPane>
-      </TabContent>
-    </div>
-  </TabContainer>;
+export default class Layout extends React.Component {
+
+  render() {
+    return <TabContainer generateChildId={ ( key, type ) => type + "-" + key }>
+      <div>
+        <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Toggle
+              data-toggle="collapse" data-target="#navbar"/>
+            <Navbar.Brand>
+              <a href="#">
+                <span><Image className="logo_img_small" src="/ONR_Logo.jpg"/></span>
+                <span>&ensp;</span>
+                <span>Title</span>
+              </a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Navbar.Collapse id="navbar">
+            <Nav>
+              <NavItem eventKey="scenarios">Scenarios</NavItem>
+              <NavItem eventKey="sessions">Sessions</NavItem>
+              <NavItem eventKey="review">Review</NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <TabContent animation={ false }>
+          <TabPane eventKey="scenarios"><Scenarios/></TabPane>
+          <TabPane eventKey="sessions"><Sessions/></TabPane>
+          <TabPane eventKey="review"><Review/></TabPane>
+        </TabContent>
+      </div>
+    </TabContainer>;
+  }
+
 }
